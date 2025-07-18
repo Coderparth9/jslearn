897bfd7299279a86e55bd6b5d2c3d1b8be5d449e
Coderparth9 <parthmaharishi77@gmail.com>
2025-07-18 16:41:31 +0530

    learn function,object,string,event

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ function add(c, d) {
 console.log(add(5, 10));
 function myfunction() {
     console.log("Hello, this is my function!");
-    
-}
+    }
 myfunction();
 
 function square(num) {
@@ -102,3 +101,77 @@ let student1 = new Student("Parth", 20);
 let student2 = new Student("Dev", 21);
 student1.intro();
 student2.intro();
+
+function stud(name, age){
+    this.name = name;
+    this.age =age;
+    this.init = function() {
+        console.log(name, age);
+    }; 
+}   
+let stud1 = new stud("harish", 22);
+let stud2 = new stud("surya", 23);
+stud1.init();
+stud2.init();   
+
+
+//strings in javascript
+
+let str = "Hello, World!";
+console.log("Original String: ", str);
+console.log("Length of String: ", str.length);//lenght of string
+console.log("Uppercase: ", str.toUpperCase());//uppercase conversion
+console.log("Lowercase: ", str.toLowerCase());//lowercase conversion
+
+let txt = `this is let code practice for javascript`;
+console.log("Original Text: ", txt);
+
+
+let numbers = "121324464789";
+console.log(numbers.slice(0, 7));//slice method
+
+let text = 
+`The quick
+brown fox          //multiline string
+jumps over
+the lazy dog`;
+console.log(text);
+let char = text.charAt( 5);//character at method
+console.log(char);
+
+let con="Hello ";
+let con1="World!";
+let con2 = " How are you?";
+console.log(con2[0],con2[10]);
+
+let con3 = con.concat(con1,con2);
+console.log("Concatenated String: ", con3);//concatenation of strings
+console.log(con3.slice(6, 12)); //sclice method
+console.log(con3.length)
+
+let str1 = "Hello, World! ";
+console.log(str1.length);
+console.log(str1[0]); //first character
+console.log(str1.includes("Hello")); //check string includes a word or not
+console.log(str1.repeat(5));  //repeat method
+console.log(str1.replace("World", "JavaScript")); //replace method
+
+let str2 = "JavaScript is fun fun fun !";
+console.log(str2.length);
+console.log("before replace: " +str2);
+console.log("After replaced: " +str2.replace(/fun/g, "funny")); //replace all occurrences of "fun" with "funny"
+console.log(str2.split(" ")); //split method by space
+console.log(str2.indexOf("fun")); //index of method
+console.log(str2.lastIndexOf("fun")); //last index of method
+console.log(str2.search("is")); //search method
+
+function reverseString(str) { //reverse string function
+return str.split("").reverse().join("");
+}
+console.log(reverseString("Hello, World!"));
+
+//number in javascript
+let number3 = 123e-6,
+    number4 = 123e2,
+    number5 = 123e2;
+console.log("Exponential Notation: ", number3,number4, number5);
