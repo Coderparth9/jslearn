d77e96522f7c810a3a5a8e88351bd65368606b50
Coderparth9 <parthmaharishi77@gmail.com>
2025-07-18 12:34:07 +0530

    lLearning Function,Object

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
-js
+// Functions and objects in JavaScript
+
+//Functions
+console.log("hello");
+
+function add(c, d) {
+    return c + d;
+}
+console.log(add(5, 10));
+function myfunction() {
+    console.log("Hello, this is my function!");
+    
+}
+myfunction();
+
+function square(num) {
+    console.log("square is " + num * num);
+}
+square(5);  
+
+let arrowsquare = (a) => {
+    console.log(+a + " Arrow function square is " + a * a);
+    
+}
+arrowsquare(7);
+
+function Product(a,b) {
+    return a * b;
+    
+}
+Product(5, 6);
+console.log(Product(6, 6));
+
+
+function Sum(x,y,z) {
+    console.log("Sum is " + (x + y + z));
+}
+Sum(5,6,15);
+
+
+
+//Objects in javascript
+
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+}
+console.log("Old Car: ", car);
+
+car.brand = "BMW";
+car.model = "M4 Competition";
+car.year = 2023;
+console.log("New Car: ", car);
+
+
+let person = {
+    name: "John",
+    age: 30,
+    gender: "Male"
+};
+console.log("Person: ", person);
+
+person.age = 31;
+console.log("Age after 1 year: ", person);
+
+person.number = 1234567890;
+console.log("Person new number: ", person);
+
+delete person.gender;
+console.log("Person after deleting gender", person);
+
+person.car = {
+    car1: "Polo",
+    car2: "Civic",
+};
+console.log("Person with car: ", person);
+
+
+let student = {
+    name: "Alice",
+    age: 20,
+    hello: function() {
+        console.log("Hello, my name is " + this.name);
+    },
+
+    Hey: function(){
+        console.log("Hey, I am " + this.name);
+    }
+};
+student.hello();
+student.Hey();
